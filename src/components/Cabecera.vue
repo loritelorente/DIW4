75be8b3fa1ec59f21b28280cd60148ce61b5bd7e
juan <curl -sL https://deb.nodesource.com/setup_16.x |>
2022-01-12 18:16:33 +0100

    first commit

---
+++ src/components/Cabecera.vue
@@ -0,0 +1,18 @@
+<template>
+    <header id="Cabecera">
+        <h1>Cabecera de la página</h1>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#Cabecera h1 {
+    background-color: lightgrey;
+    font-size: 3rem;
+}
+</style>
